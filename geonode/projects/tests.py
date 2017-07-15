8e06aeb7391632077201c62c285a15a13d1faa05
jodom <jodomouma@gmail.com>
2017-07-15 14:34:00 +0300

    Test when there are no projects

--- geonode/projects/tests.py
+++ geonode/projects/tests.py
@@ -3,9 +3,6 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.test import TestCase
 
-# from .views import list_projects, lis_complete_projects, \
-#     list_ongoing_projects, project_detail, new_project, project_edit
-
 from . import views
 from .models import Project
 
@@ -21,6 +18,14 @@ class AllProjectsPageTest(TestCase):
     def test_project_list_page_uses_correct_template(self):
         response = self.client.get('/projects/')
         self.assertTemplateUsed(response, 'projects/projects.html')
+
+    def test_reply_when_no_projects_created(self):
+        self.assertEqual(Project.objects.count(), 0)
+
+        response = self.client.get('/projects/')
+        self.assertContains(
+            response,
+            'There are no projects created yet.')
 
     def test_list_all_projects(self):
         ongoing_project = Project.objects.create(
@@ -46,6 +51,14 @@ class OngoingProjectsPageTest(TestCase):
         response = self.client.get('/projects/ongoing/')
         self.assertTemplateUsed(response, 'projects/projects.html')
 
+    def test_reply_when_no_ongoing_project(self):
+        self.assertEqual(Project.objects.count(), 0)
+
+        response = self.client.get('/projects/ongoing/')
+        self.assertContains(
+            response,
+            'There are no continuing projects currently.')
+
     def test_list_only_ongoing_projects(self):
         ongoing_project = Project.objects.create(
             title='Test ongoing project', status='ON',
@@ -69,6 +82,14 @@ class CompletedProjectsPageTest(TestCase):
     def test_project_list_cp_page_uses_correct_template(self):
         response = self.client.get('/projects/completed/')
         self.assertTemplateUsed(response, 'projects/projects.html')
+
+    def test_reply_when_no_completed_project(self):
+        self.assertEqual(Project.objects.count(), 0)
+
+        response = self.client.get('/projects/completed/')
+        self.assertContains(
+            response,
+            'There are no completed projects yet.')
 
     def test_list_only_completed_projects(self):
         ongoing_project = Project.objects.create(
